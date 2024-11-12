236041e59277467556110b6c87ac1e48ba9d2c98
TioJavi <keima13234@hotmail.com>
2024-11-12 17:24:48 -0400

    Creando Endpoint de Categorias Get

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoriaCollection;
+
+class CategoriaController extends Controller
+{
+   public function index(){
+    return new CategoriaCollection(Categoria::all());
+   }
+}
