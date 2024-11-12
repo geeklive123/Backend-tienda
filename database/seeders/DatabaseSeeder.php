e495eca4bfdc4d7c7fdb4d54d32f92a7a0500e82
TioJavi <keima13234@hotmail.com>
2024-11-12 18:12:23 -0400

    Creando Endpoint de ProductosGet

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
        
 
         $this->call(CategoriaSeeder::class);
+        $this->call(ProductoSeeder::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('/categorias',CategoriaController::class);
+Route::apiResource('/productos',ProductoController::class);
